36b9d778a8bb44b51649d036bf42d58223fbddf3
kapil sharma <urfusion@users.noreply.github.com>
2017-01-18 17:07:21 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+/*******EDIT LINES 3-8*******/
+$DB_Server = "localhost"; //MySQL Server    
+$DB_Username = ""; //MySQL Username     
+$DB_Password = "";             //MySQL Password     
+$DB_DBName = "";         //MySQL Database Name  
+$DB_TBLName = "tablename"; //MySQL Table Name   
+$filename = "Student_export-".time();         //File Name
+/*******YOU DO NOT NEED TO EDIT ANYTHING BELOW THIS LINE*******/    
+//create MySQL connection   
+$sql = "SELECT * FROM  ".$DB_TBLName ;
+$Connect = @mysql_connect($DB_Server, $DB_Username, $DB_Password) or die("Couldn't connect to MySQL:&lt;br&gt;" . mysql_error() . "&lt;br&gt;" . mysql_errno());
+//select database   
+$Db = @mysql_select_db($DB_DBName, $Connect) or die("Couldn't select database:&lt;br&gt;" . mysql_error(). "&lt;br&gt;" . mysql_errno());   
+//execute query
+$result = @mysql_query($sql,$Connect) or die("Couldn't execute query:&lt;br&gt;" . mysql_error(). "&lt;br&gt;" . mysql_errno());    
+$file_ending = "xls";
+//header info for browser
+header("Content-Type: application/xls");    
+header("Content-Disposition: attachment; filename=$filename.xls");  
+header("Pragma: no-cache");
+header("Expires: 0");
+/*******Start of Formatting for Excel*******/   
+//define separator (defines columns in excel &amp; tabs in word)
+$sep = "\t"; //tabbed character
+//start of printing column names as names of MySQL fields
+for ($i = 0; $i &lt; mysql_num_fields($result); $i++) {
+echo mysql_field_name($result,$i) . "\t";
+}
+print("\n");    
+//end of printing column names  
+//start while loop to get data
+    while($row = mysql_fetch_row($result))
+    {
+        $schema_insert = "";
+        for($j=0; $j&lt;mysql_num_fields($result);$j++)
+        {
+            if(!isset($row[$j]))
+                $schema_insert .= "NULL".$sep;
+            elseif ($row[$j] != "")
+                $schema_insert .= "$row[$j]".$sep;
+            else
+                $schema_insert .= "".$sep;
+        }
+        $schema_insert = str_replace($sep."$", "", $schema_insert);
+        $schema_insert = preg_replace("/\r\n|\n\r|\n|\r/", " ", $schema_insert);
+        $schema_insert .= "\t";
+        print(trim($schema_insert));
+        print "\n";
+    }  
+    die;
